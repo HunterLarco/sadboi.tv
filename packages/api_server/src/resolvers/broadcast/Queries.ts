2b6c2ae9b0c7a175846de1794f1b081144264a15
Hunter Larco <hunter@larcolabs.com>
2023-01-05 11:57:34 -0500

    fix pagination bugs

--- packages/api_server/src/resolvers/broadcast/Queries.ts
+++ packages/api_server/src/resolvers/broadcast/Queries.ts
@@ -2,17 +2,16 @@ import type { QueryResolvers } from '@generated/graphql/broadcast_service/resolv
 
 export const resolvers: QueryResolvers = {
   async broadcastEventHistory(_0, { request }, { dataSources }) {
-    const kPageSize = 101;
+    const kPageSize = 100;
 
     const history = await dataSources.BroadcastEvent.getHistory({
       startFrom: request.cursor ?? undefined,
-      pageSize: kPageSize,
+      pageSize: kPageSize + 1,
     });
 
     return {
-      events: history.slice(0, -1),
-      nextPageCursor:
-        history.length == kPageSize ? history[kPageSize - 1].id : null,
+      events: history.slice(0, kPageSize),
+      nextPageCursor: history.length > kPageSize ? history[kPageSize].id : null,
     };
   },
 };
