560c1e6566f5643f3363466b339b9b67a30213c6
Hunter Larco <hunter@larcolabs.com>
2023-01-07 18:00:52 -0500

    working player handoff

--- packages/frontend/src/components/MediaPlayer.vue
+++ packages/frontend/src/components/MediaPlayer.vue
@@ -1,14 +1,51 @@
 <script setup lang="ts">
 import '@mux/mux-player';
+
+/*
+import type MuxPlayerElement from '@mux/mux-player';
+import { computed, ref, shallowRef } from 'vue';
+
+/// Player Management
+///
+/// We always retain two players, one currently playing and one up next. This
+/// way we can buffer content prior to it playing.
+
+const playerA = shallowRef<MuxPlayerElement>();
+const playerB = shallowRef<MuxPlayerElement>();
+
+const useFirstPlayer = ref(false);
+const currentPlayer = computed(() =>
+  useFirstPlayer.value ? playerA.value! : playerB.value!
+);
+const upNextPlayer = computed(() =>
+  useFirstPlayer.value ? playerB.value! : playerA.value!
+);
+
+/// Actions
+
+function queueNextMedia() {
+  const player = upNextPlayer.value;
+  player.playbackId = 'S5n1D6XcFsEpc00ae01fILuJXUPEkwdJUb01uIqSfUKlbM';
+}
+
+function playNext() {
+  currentPlayer.value!.pause();
+  upNextPlayer.value!.play();
+  useFirstPlayer.value = !useFirstPlayer.value;
+}
+*/
 </script>
 
 <template>
   <div class="MediaPlayer">
+    <!--
+    <mux-player stream-type="on-demand" ref="playerA" v-show="useFirstPlayer" />
     <mux-player
-      playback-id="S5n1D6XcFsEpc00ae01fILuJXUPEkwdJUb01uIqSfUKlbM"
-      autoplay
       stream-type="on-demand"
+      ref="playerB"
+      v-show="!useFirstPlayer"
     />
+    -->
   </div>
 </template>
 
